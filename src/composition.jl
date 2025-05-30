48d5b8d2156a30d7b1cbc5f501fb42720bbe872a
Sergio Sánchez Ramírez <15837247+mofeing@users.noreply.github.com>
2025-05-30 16:05:38 +0900

    Remove `domain`, `image` functions (#28)
    * Remove `domain`, `image` functions
    
    * Remove `domain`, `image` tests
    
    * Stop exporting the symbols
    
    * Update docs
    
    * Fix composition
    
    * Document changes on CHANGELOG
    
    * Be more detailed in the CHANGELOG
    
    * Add headings to CHANGELOG
    
    * Update src/composition.jl
    
    ---------
    
    Co-authored-by: Michael Goerz <mail@michaelgoerz.net>

--- src/composition.jl
+++ src/composition.jl
@@ -2,19 +2,12 @@
     (*)(a::Bijection{A,B}, b::Bijection{B,C})::Bijection{A,C} where {A,B,C}
 
 The result of `a * b` is a new `Bijection` `c` such that `c[x]` is `a[b[x]]` for `x`
-in the domain of `b`. This function throws an error is `domain(a)` is not the same 
-as the `image` of `b`.    
+in the domain of `b`.
 """
-function Base.:(*)(a::Bijection{B,A}, b::Bijection{C,B})::Bijection{C,A} where {A,B,C}
-    # check that the domain of a equals the image of B
-    if Set(domain(a)) != Set(image(b))
-        error("Domain/Image mismatch")
-    end
-
+function Base.:(*)(a::Bijection{B,A}, b::Bijection{C,B}) where {A,B,C}
     c = Bijection{C,A}()
-    for x in domain(b)
+    for x in keys(b)
         c[x] = a[b[x]]
     end
-
     return c
 end
